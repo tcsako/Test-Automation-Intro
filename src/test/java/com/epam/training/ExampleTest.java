523231abce9e0e5ef1d76e9d1715e0ef41e78911
unknown <Guest@test.epam.com>
2015-11-17 20:12:32 +0100

    no message

--- src/test/java/com/epam/training/ExampleTest.java
+++ src/test/java/com/epam/training/ExampleTest.java
@@ -1,9 +1,13 @@
 package com.epam.training;
 
 import org.junit.After;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,11 +18,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  *
  * @author Zsolt_Horvath1
  */
+@RunWith(Parameterized.class)
 public class ExampleTest {
 
     /** The {@link WebDriver} instance that will be used during the test. */
     private WebDriver driver;
-
+    
+   
+    @Parameter(0)
+    public String searchText;
+    @Parameter(1)
+    public String number;
+    @Parameter(2)
+    public String bool;
     /**
      * Browser initialization before the test.
      */
@@ -36,18 +48,18 @@ public class ExampleTest {
      * 2. Type 'EPAM' into the search input field
      * 3. Submit the form
      * 4. Check whether the title of the browser contains the word 'EPAM'
+     * @throws InterruptedException 
      */
     @Test
-    public void testExample() {
+    public void testExample() throws InterruptedException {
         driver.get("http://www.szeged.hu");
 
-        WebElement element = driver.findElement(By.name("q"));
+        WebElement element = driver.findElement(By.name("a"));
         element.sendKeys("EPAM");
-
-        element.submit();
-
-        Assert.assertTrue(driver.getTitle().contains("EPAM"));
+       element.submit();
+       Assert.assertTrue(driver.getTitle().contains("EPAM"));
     }
+    
 
     /**
      * Closes the browser after the test.
@@ -56,5 +68,14 @@ public class ExampleTest {
     public void tearDown() {
         driver.quit();
     }
+    @Parameters
+    public static Object[][] searchParams() {
+    	return new Object [][] {
+    		{ "EPAM"},
+   //		{"Mozi"},
+    //		{"Medvedisznóember"},
+    	};
+    }
+    
 
 }
