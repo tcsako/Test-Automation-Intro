b42e22ef5dbc557f3374daacc5ff95b2876a1df0
unknown <Guest@test.epam.com>
2015-11-17 20:13:26 +0100

    experiment with parameters

--- src/test/java/com/epam/training/ExampleTest.java
+++ src/test/java/com/epam/training/ExampleTest.java
@@ -1,23 +1,40 @@
 package com.epam.training;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import Webelement;
 
 /**
  * Example test(s) to demonstrate Selenium WebDriver.
  *
  * @author Zsolt_Horvath1
  */
+
+@RunWith(Parameterized.class)
 public class ExampleTest {
 
     /** The {@link WebDriver} instance that will be used during the test. */
     private WebDriver driver;
+    
+    @Parameter(0)
+    public String searcText;
+    @Parameter(1)
+    public String searcTectScnd;
 
     /**
      * Browser initialization before the test.
@@ -39,14 +56,21 @@ public class ExampleTest {
      */
     @Test
     public void testExample() {
-        driver.get("http://www.szeged.hu");
+       // driver.get("http://www.szeged.hu");
+    	driver.get("http://www.google.com");
 
         WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("EPAM");
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // arra valo, hogy ha action utan mindenfelekeppen varjon x ideig
+        //driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS); // hajlando vagyok varni a pageloadra
+        
+        element.sendKeys(searcText);
 
         element.submit();
-
-        Assert.assertTrue(driver.getTitle().contains("EPAM"));
+        
+        WebElement breadcrumbelement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("breadcumb")));
+        //WebElement breadcrumbelement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
+        
+        Assert.assertTrue(driver.getTitle().contains(searcText));
     }
 
     /**
@@ -54,7 +78,21 @@ public class ExampleTest {
      */
     @After
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
+    	driver.close();
+    }
+    
+    @Parameters
+    public static Object[][] searchParams() {
+    	
+    	return new Object [][]{
+    		
+    		{"EPAM", "MOZI"},
+    		//{"MOZI" , "MOZI2"},
+    		//{"BLIZZARD" ,"MOZI3"},
+    		
+    	};
+    
     }
 
 }
