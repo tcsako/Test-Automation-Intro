2587efd9040d46b1ddc376f209946fa592e9c05d
NandorRigo <rigo.nandor@gmail.com>
2015-11-18 22:06:51 +0100

    Added sample project and corrected repo URL in Readme.md

---
+++ src/test/java/com/epam/training/po/example/BasePage.java
@@ -0,0 +1,26 @@
+package com.epam.training.po.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * This class represents the Parent class for the PageObject Framework
+ * It creates the WebDriver instance, so you don't have to instantiate it on every PageObject class
+ * 
+ * @author Nandor_Rigo
+ *
+ */
+
+public class BasePage {
+
+	private WebDriver driver;
+
+	public BasePage(WebDriver driver) {
+	        this.driver = driver;
+	        PageFactory.initElements(this.driver, this);
+	    }
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+}
