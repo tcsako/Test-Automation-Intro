b008a5e8a068d47d330c5958f35cd3893d62a5f5
unknown <Guest@test.epam.com>
2015-11-19 20:27:55 +0100

    Added test framework by me (11.19)

--- src/test/java/com/epam/training/po/lesson/HomePage.java
+++ src/test/java/com/epam/training/po/lesson/HomePage.java
@@ -1,6 +1,8 @@
 package com.epam.training.po.lesson;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * This class represents the Home page of the PageObject Framework
@@ -11,7 +13,28 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
 	
+	@FindBy(id = "gh-ac")
+	private WebElement searchField;
 	
+	@FindBy(id = "gh-btn")
+	private WebElement searchButton;
+	
+	public HomePage fillSearch(String keyword) {
+		
+		fillSearchBox(keyword);
+		return new HomePage(getDriver());
+		
+	}
+	
+	public SearchPage submitForm() {
+		
+		searchButton.click();
+		return new SearchPage(getDriver());
+	}
+	
+	private void fillSearchBox(String keyword) {
+		searchField.sendKeys(keyword);
+	}
 	
 	public HomePage(WebDriver driver) {
 		super(driver);
