69566cd1b0f3fa1dcbb9155e260f916665ffd83c
unknown <Guest@test.epam.com>
2015-11-19 18:42:52 +0100

    Training0 update

--- src/test/java/com/epam/training/Trainging0.java
+++ src/test/java/com/epam/training/Trainging0.java
@@ -1,6 +1,6 @@
 package com.epam.training;
 
-import static org.junit.Assert.*;
+
 
 import java.util.List;
 
@@ -9,7 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class Trainging0 {
 
